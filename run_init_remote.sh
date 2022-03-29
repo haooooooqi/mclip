f662652ce6f9fd9022f673563c280d5df47c5ba2
KaimingHe <kmh.kaiminghe@gmail.com>
2022-03-29 18:05:19 +0000

    script

--- run_init_remote.sh
+++ run_init_remote.sh
@@ -5,8 +5,12 @@ REPO=https://${GITHUB_ID}@github.com/KaimingHe/flax_dev.git
 BRANCH=main
 
 gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
+    --worker=0 --command "
+pip3 list | grep 'jax\|flax\|tensorflow '
+"
+
+gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
     --worker=all --command "
-git config --global credential.helper store
 git clone -b $BRANCH $REPO flax_dev
 
 pip install 'jax[tpu]==0.3.4' -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
