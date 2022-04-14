cdc5ed00275b6c1974b3e966a65e16751929a58e
KaimingHe <kmh.kaiminghe@gmail.com>
2022-04-14 00:12:20 +0000

    dbg: no ave log

--- run_staging.sh
+++ run_staging.sh
@@ -1,19 +1,19 @@
-VM_NAME=kmh-tpuvm-v3-128-1
-# VM_NAME=kmh-tpuvm-v3-256-3
+# VM_NAME=kmh-tpuvm-v3-128-1
+VM_NAME=kmh-tpuvm-v3-256-3
 echo $VM_NAME
 REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
 BRANCH=main
 
 # salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
 
-lr=1e-4
+lr=1.5e-4
 ep=1600
 batch=4096
 
 
 CONFIG=cfg_mae_large
 # pytorch_recipe: _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_maeDBG_batch${batch}_lr${lr}_vmap_normpix_sincos_initmaev2_cropv2_donate_olkNN_NOexClsDBG_masknoise_qkv_buf16x1024
+JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_maeDBG_batch${batch}_lr${lr}_vmap_NOnormpix_sincos_initmaev2_cropvc_donate_olkNN_NOexClsDBG_masknoise_qkv_buf16x1024_noavelog
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/home/${USER}/logs/${JOBNAME}
@@ -49,9 +49,9 @@ python3 main.py \
     --config.num_epochs=${ep} \
     --config.learning_rate=${lr} \
     --config.save_every_epochs=10 \
-    --config.model.norm_pix_loss=True \
+    --config.model.norm_pix_loss=False \
     --config.model.sincos=True \
-    --config.aug.crop_ver=v2 \
+    --config.aug.crop_ver=vc \
     --config.donate=True \
     --config.aug.make_mask_noise=True \
 
