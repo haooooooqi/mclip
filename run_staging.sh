b159246d9adf62566d9b23937c4e726f278c08a9
KaimingHe <kmh.kaiminghe@gmail.com>
2022-04-17 21:34:14 +0000

    add resume dir

--- run_staging.sh
+++ run_staging.sh
@@ -20,8 +20,10 @@ source scripts/select_chkpt_${vitsize}.sh
 
 name=`basename ${PRETRAIN_DIR}`
 
+RESUME_DIR='gs://kmh-gcp/checkpoints/flax/2021-10-26-22-16-05-v3-128-mb4096-epo1600-PMAEp16-ViTLarge-lr1e-4-wd5e-2-warm40-mask0.75-pred8d512-exNB-msaLNmlpLNeLNpLNkBN0-1view-NOrelpos-abspos-clstoken-qkv-NOlayerscale-LNtgt-resume3_convert_pt2jax_finetune/20220417_172143_kmh-tpuvm-v3-256-4_cfg_vit_large_50ep_ftpy_b1024_lr1e-3_lrd0.75_dp0.2_autoaug_shf16x256_hostbatch_seed0_mergesanity/checkpoint_50040'
+
 # finetune_pytorch_recipe (ftpy): lb0.1_b0.999_cropv4_exwd_initv2_headinit0.001_tgap_dp_mixup32_cutmix32_noerase_warmlr_minlr_autoaug
-JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_ftpy_b${batch}_lr${lr}_lrd${lrd}_dp${dp}_autoaug_shf16x256_mixbatch_seed${seed}_preload
+JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_ftpy_b${batch}_lr${lr}_lrd${lrd}_dp${dp}_autoaug_shf16x256_mixbatch_seed${seed}_resume40_fulltfds
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/home/${USER}/logs/${JOBNAME}
@@ -41,7 +43,7 @@ gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
     --worker=all --command "
 cd ~/flax_dev
 git pull
-git checkout vit.ft.tfds
+git checkout vit.ft.tfds.v2
 git pull
 git rev-parse --short HEAD
 
@@ -55,6 +57,7 @@ python3 main.py \
     --workdir=${WORKDIR} \
     --config=configs/$CONFIG.py \
     --config.pretrain_dir=${PRETRAIN_DIR} \
+    --config.resume_dir=${RESUME_DIR} \
     --config.batch_size=${batch} \
     --config.learning_rate=${lr} \
     --config.learning_rate_decay=${lrd} \
