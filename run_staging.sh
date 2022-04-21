c48b741fbceae83a611a099c9417f8c5f189b02f
KaimingHe <kmh.kaiminghe@gmail.com>
2022-04-21 06:04:46 +0000

    add staging

--- run_staging.sh
+++ run_staging.sh
@@ -1,59 +1,22 @@
 # VM_NAME=kmh-tpuvm-v3-128-1
-VM_NAME=kmh-tpuvm-v3-256-3
+VM_NAME=kmh-tpuvm-v3-256-1
 echo $VM_NAME
-REPO=https://71d519550fe3430ecbf39b70467e9210aed5da69:@github.com/KaimingHe/flax_dev.git
-BRANCH=main
 
-# salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
+# ------------------------------------------------
+# copy all files to staging
+# ------------------------------------------------
+now=`date '+%y%m%d%H%M%S'`
+export salt=`head /dev/urandom | tr -dc a-z0-9 | head -c8`
+export STAGEDIR=/kmh_data/staging/${now}-${salt}-code
 
-lr=1.0e-4
-ep=1600
-batch=4096
+sudo chmod 777 $STAGEDIR
 
+echo 'Copying files...'
+rsync -a . $STAGEDIR --exclude=tmp
+echo 'Done copying.'
 
-CONFIG=cfg_mae_large
-# pytorch_recipe: _autoaug_lb0.1_cropv4_exwd_initv2_rsinit_dp0.1_cutmixup_minlr
-JOBNAME=flax/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_maeDBG_batch${batch}_lr${lr}_vmap_normpix_sincos_initmaev2_cropv2ALTER_donate_olkNN_NOexClsDBG_buf16x1024_seed
+cd $STAGEDIR
+echo 'Current dir: '`pwd`
+# ------------------------------------------------
 
-WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
-LOGDIR=/home/${USER}/logs/${JOBNAME}
-mkdir -p ${LOGDIR}
-
-source run_init_remote.sh
-
-# check libraries
-# gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
-#     --worker=0 --command "
-# pip3 list | grep jax
-# pip3 list | grep flax
-# pip3 list | grep tensorflow
-# "
-
-gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
-    --worker=all --command "
-cd ~/flax_dev
-git pull
-git checkout mae.subtleties
-git pull
-git rev-parse --short HEAD
-
-# pip3 list | grep 'jax\|flax\|tensorflow '
-
-cd ~/flax_dev
-export TFDS_DATA_DIR=gs://kmh-gcp/tensorflow_datasets
-python3 main.py \
-    --workdir=$WORKDIR \
-    --config=configs/$CONFIG.py \
-    --config.batch_size=${batch} \
-    --config.log_every_steps=100 \
-    --config.num_epochs=${ep} \
-    --config.learning_rate=${lr} \
-    --config.save_every_epochs=50 \
-    --config.model.norm_pix_loss=True \
-    --config.model.sincos=True \
-    --config.aug.crop_ver=v2 \
-    --config.donate=True \
-
-" 2>&1 | tee $LOGDIR/pretrain.log
-
-echo ${VM_NAME}
+source run_remote.sh
