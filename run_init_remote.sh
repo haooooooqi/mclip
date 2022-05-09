c2122fdf910c17b3f0e98bef71ed588d1ef36ac0
KaimingHe <kmh.kaiminghe@gmail.com>
2022-05-09 20:35:47 +0000

    script

--- run_init_remote.sh
+++ run_init_remote.sh
@@ -1,20 +1,16 @@
 gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
     --worker=all --command "
-pip3 install tensorflow --upgrade
-"
-
-gcloud alpha compute tpus tpu-vm ssh ${VM_NAME} --zone europe-west4-a \
-    --worker=all --command "
 pip install 'jax[tpu]==0.3.4' -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install --upgrade clu
 
 pip install flax==0.4.1
 
-# pip3 install torchvision --upgrade
+pip3 install tensorflow --upgrade
 pip3 install tensorflow==2.8
 pip3 install tensorflow-probability
 pip3 install tensorflow_addons
 
+# pip3 install torchvision --upgrade
 pip3 install torch==1.7.1
 pip3 install torchvision==0.8.2
 pip3 install timm==0.4.12
