f9d033a2375da6ca7a093714c0b3edf47a550e55
KaimingHe <kmh.kaiminghe@gmail.com>
2022-06-02 23:06:14 +0000

    script

--- run_remote.sh
+++ run_remote.sh
@@ -3,7 +3,7 @@ echo 'code dir: '$STAGEDIR
 seed=0
 batch=1024
 # lr=1e-4
-wd=0.0
+wd=0.05
 lrd=1.0
 ep=50
 warm=5
@@ -13,7 +13,7 @@ beta2=0.999
 
 partitions=1
 
-pft=0
+pft=12
 
 vitsize=large
 CONFIG=cfg_vit_${vitsize}
@@ -24,7 +24,7 @@ name=`basename ${PRETRAIN_DIR}`
 
 # finetune_pytorch_recipe (ftpy): lb0.1_b0.999_cropv4_exwd_initv2_headinit0.001_tgap_dp_mixup32_cutmix32_noerase_warmlr_minlr_autoaug
 # finetune_torch_loader (fttl): randaugv2erase_TorchLoader
-JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_pdp${pdp}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_p${partitions}_pft768x${pft}_adapter_token_pred_posemb
+JOBNAME=flax/${name}_finetune/$(date +%Y%m%d_%H%M%S)_${VM_NAME}_${CONFIG}_${ep}ep_fttl_b${batch}_wd${wd}_lr${lr}_lrd${lrd}_pdp${pdp}_dp${dp}_warm${warm}_s${seed}_beta${beta2}_p${partitions}_pft768x${pft}_adapter2 # _token_pred_posemb
 
 WORKDIR=gs://kmh-gcp/checkpoints/${JOBNAME}
 LOGDIR=/kmh_data/logs/${JOBNAME}
@@ -69,7 +69,7 @@ python3 main.py \
     --config.aug.autoaug=randaugv2 \
     --config.model.transformer.droppath_rate=${dp} \
     --config.model.predictor.transformer.droppath_rate=${pdp} \
-    --config.model.predictor.transformer.num_heads=${pft} \
+    --config.model.predictor.transformer.num_layers=${pft} \
     --config.seed_tf=${seed} \
     --config.seed_jax=${seed} \
     --config.seed_pt=${seed} \
