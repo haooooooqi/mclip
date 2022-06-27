1cc3e515ea1b02ea557e30ef712723cdd795bb60
KaimingHe <kmh.kaiminghe@gmail.com>
2022-06-27 22:26:29 +0000

    script

--- scripts/select_chkpt_huge1x_p16.sh
+++ scripts/select_chkpt_huge1x_p16.sh
@@ -1,4 +1,5 @@
 # select ViT-Huge1x/16
 
 # helloworld
-PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220624_194329_maet5x_kmh-tpuvm-v3-256-3_cfg_mae_huge1x_p16_800ep_b4096_lr1e-4_mk0.75_s100_p1_re1.0_normpix_exwd_NOsplit_fastsave'
+# PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220624_194329_maet5x_kmh-tpuvm-v3-256-3_cfg_mae_huge1x_p16_800ep_b4096_lr1e-4_mk0.75_s100_p1_re1.0_normpix_exwd_NOsplit_fastsave'
+PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220626_031329_maet5x_kmh-tpuvm-v3-256-3_cfg_mae_huge1x_p16_1600ep_b4096_lr1e-4_mk0.75_s100_p1_re1.0_normpix_exwd_NOsplit_fastsave'
