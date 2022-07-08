7053a8e7c5e7bbbb799cae3cc9f85f8d4c04a86f
KaimingHe <kmh.kaiminghe@gmail.com>
2022-07-08 20:03:53 +0000

    script

--- run_staging.sh
+++ run_staging.sh
@@ -1,5 +1,5 @@
-VM_NAME=kmh-tpuvm-v3-512-1
-# VM_NAME=kmh-tpuvm-v3-256-6
+# VM_NAME=kmh-tpuvm-v3-512-1
+VM_NAME=kmh-tpuvm-v3-256-7
 echo $VM_NAME
 
 # ------------------------------------------------
@@ -19,7 +19,7 @@ cd $STAGEDIR
 echo 'Current dir: '`pwd`
 # ------------------------------------------------
 
-for seed in 0
+for seed in 2
 do
 source run_remote_in22k.sh
 
