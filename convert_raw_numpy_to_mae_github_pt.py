754406608c92f6a2f94ac22b3e3b4f688b80f886
Ronghang Hu <ronghang.hu@gmail.com>
2022-07-13 22:53:41 +0000

    use `torch.from_numpy` to save memory

--- convert_raw_numpy_to_mae_github_pt.py
+++ convert_raw_numpy_to_mae_github_pt.py
@@ -29,77 +29,77 @@ def convert_jax_t5x_to_pt(t5x_np_ckpt, remove_k_bias=False):
                 # the transformer attention's K and V weights will be handled when processing Q weights
                 continue
             elif ".LayerNorm_0.scale" in k:
-                pt_ckpt[pt_k_prefix + "norm1.weight"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "norm1.weight"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".LayerNorm_0.bias" in k:
-                pt_ckpt[pt_k_prefix + "norm1.bias"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "norm1.bias"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".LayerNorm_1.scale" in k:
-                pt_ckpt[pt_k_prefix + "norm2.weight"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "norm2.weight"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".LayerNorm_1.bias" in k:
-                pt_ckpt[pt_k_prefix + "norm2.bias"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "norm2.bias"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".MlpBlock_0.Dense_0.kernel" in k:
-                pt_ckpt[pt_k_prefix + "mlp.fc1.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(1, 0)
+                pt_ckpt[pt_k_prefix + "mlp.fc1.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(1, 0)
             elif ".MlpBlock_0.Dense_0.bias" in k:
-                pt_ckpt[pt_k_prefix + "mlp.fc1.bias"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "mlp.fc1.bias"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".MlpBlock_0.Dense_1.kernel" in k:
-                pt_ckpt[pt_k_prefix + "mlp.fc2.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(1, 0)
+                pt_ckpt[pt_k_prefix + "mlp.fc2.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(1, 0)
             elif ".MlpBlock_0.Dense_1.bias" in k:
-                pt_ckpt[pt_k_prefix + "mlp.fc2.bias"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "mlp.fc2.bias"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".MultiHeadDotProductAttention_0.out.kernel" in k:
-                pt_ckpt[pt_k_prefix + "attn.proj.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(1, 0)
+                pt_ckpt[pt_k_prefix + "attn.proj.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(1, 0)
             elif ".MultiHeadDotProductAttention_0.out.bias" in k:
-                pt_ckpt[pt_k_prefix + "attn.proj.bias"] = torch.Tensor(t5x_np_ckpt[k])
+                pt_ckpt[pt_k_prefix + "attn.proj.bias"] = torch.from_numpy(t5x_np_ckpt[k])
             elif ".MultiHeadDotProductAttention_0.query.kernel" in k:
                 pt_ckpt[pt_k_prefix + "attn.qkv.weight"] = torch.cat(
                     [
-                        torch.Tensor(t5x_np_ckpt[k]),
-                        torch.Tensor(t5x_np_ckpt[_replace(k, ".query.", ".key.")]),
-                        torch.Tensor(t5x_np_ckpt[_replace(k, ".query.", ".value.")]),
+                        torch.from_numpy(t5x_np_ckpt[k]),
+                        torch.from_numpy(t5x_np_ckpt[_replace(k, ".query.", ".key.")]),
+                        torch.from_numpy(t5x_np_ckpt[_replace(k, ".query.", ".value.")]),
                     ],
                     dim=1,
                 ).permute(1, 0)
             elif ".MultiHeadDotProductAttention_0.query.bias" in k:
                 if remove_k_bias:
-                    pt_ckpt[pt_k_prefix + "attn.q_bias"] = torch.Tensor(t5x_np_ckpt[k])
-                    pt_ckpt[pt_k_prefix + "attn.v_bias"] = torch.Tensor(t5x_np_ckpt[_replace(k, ".query.", ".value.")])
+                    pt_ckpt[pt_k_prefix + "attn.q_bias"] = torch.from_numpy(t5x_np_ckpt[k])
+                    pt_ckpt[pt_k_prefix + "attn.v_bias"] = torch.from_numpy(t5x_np_ckpt[_replace(k, ".query.", ".value.")])
                 else:
                     pt_ckpt[pt_k_prefix + "attn.qkv.bias"] = torch.cat(
                         [
-                            torch.Tensor(t5x_np_ckpt[k]),
-                            torch.Tensor(t5x_np_ckpt[_replace(k, ".query.", ".key.")]),
-                            torch.Tensor(t5x_np_ckpt[_replace(k, ".query.", ".value.")]),
+                            torch.from_numpy(t5x_np_ckpt[k]),
+                            torch.from_numpy(t5x_np_ckpt[_replace(k, ".query.", ".key.")]),
+                            torch.from_numpy(t5x_np_ckpt[_replace(k, ".query.", ".value.")]),
                         ],
                         dim=0,
                     )
             else:
                 raise Exception(f"unknown T5X param key: {k}")
         elif k == "target.cls":
-            pt_ckpt["cls_token"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["cls_token"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.posembed_encoder.pos_embedding":
-            pt_ckpt["pos_embed"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["pos_embed"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.embedding.kernel":
-            pt_ckpt["patch_embed.proj.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(3, 2, 0, 1)
+            pt_ckpt["patch_embed.proj.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(3, 2, 0, 1)
         elif k == "target.embedding.bias":
-            pt_ckpt["patch_embed.proj.bias"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["patch_embed.proj.bias"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.Transformer.encoder_norm.scale":
-            pt_ckpt["norm.weight"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["norm.weight"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.Transformer.encoder_norm.bias":
-            pt_ckpt["norm.bias"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["norm.bias"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.mask_token":
-            pt_ckpt["mask_token"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["mask_token"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.posembed_decoder.pos_embedding":
-            pt_ckpt["decoder_pos_embed"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["decoder_pos_embed"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.TransformerDecoder.decoder_norm.scale":
-            pt_ckpt["decoder_norm.weight"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["decoder_norm.weight"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.TransformerDecoder.decoder_norm.bias":
-            pt_ckpt["decoder_norm.bias"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["decoder_norm.bias"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.bottleneck.kernel":
-            pt_ckpt["decoder_embed.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(1, 0)
+            pt_ckpt["decoder_embed.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(1, 0)
         elif k == "target.bottleneck.bias":
-            pt_ckpt["decoder_embed.bias"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["decoder_embed.bias"] = torch.from_numpy(t5x_np_ckpt[k])
         elif k == "target.pred.kernel":
-            pt_ckpt["decoder_pred.weight"] = torch.Tensor(t5x_np_ckpt[k]).permute(1, 0)
+            pt_ckpt["decoder_pred.weight"] = torch.from_numpy(t5x_np_ckpt[k]).permute(1, 0)
         elif k == "target.pred.bias":
-            pt_ckpt["decoder_pred.bias"] = torch.Tensor(t5x_np_ckpt[k])
+            pt_ckpt["decoder_pred.bias"] = torch.from_numpy(t5x_np_ckpt[k])
         else:
             raise Exception(f"unknown T5X param key: {k}")
 
