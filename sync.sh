a7d02dab52a05ae28938d5dc13db6521764ace07
Xinlei Chen <xinleic@fb.com>
2022-08-03 14:59:57 -0700

    init infra

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 DEST_RUN=/checkpoint/xinleic/mae_jax/repo_mae
 DEST_DEV=mae_jax
 
-for i in configs scripts t5x utils; do 
+for i in configs scripts infra t5x utils; do 
     rsync -aiz --delete --partial --progress $i/ devtpux4:$DEST_RUN/$i/
     rsync -aiz --delete --partial --progress $i/ devtpux4:$DEST_DEV/$i/
 done
