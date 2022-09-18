7e3f43b4f078b9c5b128f5c9d9077e692074ea33
KaimingHe <kmh.kaiminghe@gmail.com>
2022-09-18 22:40:22 +0000

    script

--- scripts/select_chkpt_large.sh
+++ scripts/select_chkpt_large.sh
@@ -152,4 +152,9 @@
 # PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220916_050059_kmh-tpuvm-v3-256-4_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off0_dec8_row'
 # PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220916_045816_kmh-tpuvm-v3-256-6_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_NOnormpix_ohem0_off0_dec8_row'
 
-PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220917_052140_kmh-tpuvm-v3-256-4_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off1_dec8_row'
+# PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220917_052140_kmh-tpuvm-v3-256-4_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off1_dec8_row'
+PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220917_161629_kmh-tpuvm-v3-256-6_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off2_dec8_row'
+
+# PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220918_010035_kmh-tpuvm-v3-256-2_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off0_dec8_p2x'
+
+# PRETRAIN_DIR='gs://kmh-gcp/checkpoints/flax/20220918_070624_kmh-tpuvm-v3-512-1_cfg_mae_large_autoreg_800ep_b4096_lr1.0e-4_TorchLoader_wseed100_normpix_ohem0_off0_dec8_raster_out'  # pred_out is bugged in this run
